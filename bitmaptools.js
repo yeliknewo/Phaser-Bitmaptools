dc76d9fbe02bfff1b07d5fb4601b928d96e234bc
Yelik <insuferior@gmail.com>
2015-05-17 10:56:24 -0400

    added rectangles, fixed bugs

--- bitmaptools.js
+++ bitmaptools.js
@@ -3,9 +3,13 @@ module.exports = {
 		this.game = game;
 	},
 	
-	drawPolygonOn:function(bmd, polygon, strokeHex, fillHex, rads){
+	drawPolygonOn:function(bmd, polygon, strokeHex, fillHex, rads, addPolygon){
+		if(typeof addPolygon == 'undefined'){
+			addPolygon = false;
+		}
 		polygon = this.rotatePolygon(polygon, rads);
 		var points = polygon.toNumberArray();
+
 		bmd.ctx.fillStyle = fillHex;
 		bmd.ctx.strokeStyle = strokeHex;
 		bmd.ctx.beginPath();
@@ -14,28 +18,56 @@ module.exports = {
 			bmd.ctx.lineTo(points[i], points[i+1]);
 		}
 		bmd.ctx.lineTo(points[0], points[1]);
-		bmd.ctx.fill();
-		bmd.ctx.stroke();
+		if(fillHex != 'clear'){
+			bmd.ctx.fill();
+		}
+		if(strokeHex != 'clear'){
+			bmd.ctx.stroke();
+		}
 		bmd.ctx.closePath();
-		
+		if(addPolygon){
+			bmd.polygons.push(polygon);
+		}
 		return bmd;
 	},
 	
-	drawIsoTriangle:function(x, y, width, height, strokeHex, fillHex, rads){
+	drawIsoTriangle:function(x, y, width, height, strokeHex, fillHex, rads, addPolygon){
 		var polygon = this.makeIsoTrianglePolygon(x, y, width, height);
 		polygon = this.rotatePolygon(polygon, rads);
-		var rect = this.getPolygonRect(polygon);
-		return this.drawPolygonOn(this.game.add.bitmapData(rect[0] + rect[2], rect[1] + rect[3]), polygon, strokeHex, fillHex, 0);
+		return this.drawPolygonOn(this.makeBmd(polygon), polygon, strokeHex, fillHex, 0, addPolygon);
 	},
 	
-	drawIsoTriangleOn:function(bmd, x, y, width, height, strokeHex, fillHex, rads){
+	drawRect:function(x, y, width, height, strokeHex, fillHex, rads, addPolygon){
+		var polygon = this.makeRect(x,y,width,height);
+		polygon = this.rotatePolygon(polygon, rads);
+		return this.drawPolygonOn(this.makeBmd(polygon), polygon, strokeHex, fillHex, 0, addPolygon);
+	},
+	
+	drawIsoTriangleOn:function(bmd, x, y, width, height, strokeHex, fillHex, rads, addPolygon){
 		var polygon = this.makeIsoTrianglePolygon(x, y, width, height);
 		polygon = this.rotatePolygon(polygon, rads);
-		return this.drawPolygonOn(bmd, polygon, strokeHex, fillHex, 0);
+		return this.drawPolygonOn(bmd, polygon, strokeHex, fillHex, 0, addPolygon);
 	},
 	
+	drawRectOn:function(bmd, x, y, width, height, strokeHex, fillHex, rads, addPolygon){
+		var polygon = this.makeRect(x,y,width,height);
+		polygon = this.rotatePolygon(polygon, rads);
+		return this.drawPolygonOn(bmd, polygon, strokeHex, fillHex, 0, addPolygon);
+	},
+		
 	makeIsoTrianglePolygon:function(x, y, width, height){
 		return new Phaser.Polygon([x,y,width + x, height / 2 + y, x, height + y]);
+	},
+	
+	makeRect:function(x,y,width,height){
+		return new Phaser.Polygon([x,y,x,height,width,height,width,y]);
+	},
+	
+	makeBmd:function(polygon){
+		var rect = this.getPolygonRect(polygon);
+		var bmd = this.game.make.bitmapData(rect.width, rect.height);
+		bmd.polygons = [];
+		return bmd;
 	},
 	
 	getPolygonRect:function(polygon){
@@ -53,7 +85,7 @@ module.exports = {
 			y1 = this.game.math.max(y1, points[y]);
 		}
 		
-		return [x0, y0, x1-x0, y1-y0];
+		return new Phaser.Rectangle(x0, y0, x1-x0, y1-y0);[x0, y0, x1-x0, y1-y0];
 	},
 	
 	rotatePolygon:function(polygon, rads){
@@ -62,15 +94,14 @@ module.exports = {
 		}
 		var points = polygon.toNumberArray();
 		var rect = this.getPolygonRect(polygon);
-		var center = [rect[2] / 2 + rect[0], rect[3] / 2 + rect[1]];
 		var x0 = points[0], y0 = points[1];
 		
 		for(var i = 0;i<points.length;i+=2){
-			var theta = this.game.math.angleBetween(center[0], center[1], points[i], points[i+1]);
-			var distance = this.game.math.distance(points[i], points[i+1], center[0], center[1]);
+			var theta = this.game.math.angleBetween(rect.centerX, rect.centerY, points[i], points[i+1]);
+			var distance = this.game.math.distance(points[i], points[i+1], rect.centerX, rect.centerY);
 			theta += rads;
-			points[i] = this.game.math.roundTo(Math.cos(theta) * distance, -2) + center[0];
-			points[i+1] = this.game.math.roundTo(Math.sin(theta) * distance, -2) + center[1];
+			points[i] = this.game.math.roundTo(Math.cos(theta) * distance, -2) + rect.centerX;
+			points[i+1] = this.game.math.roundTo(Math.sin(theta) * distance, -2) + rect.centerY;
 			x0 = this.game.math.min(x0, points[i]);
 			y0 = this.game.math.min(y0, points[i+1]);
 		}
